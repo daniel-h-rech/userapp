fd1e345650c7a3bec8b4019c076dde657bf704ae
Daniel Haeser Rech <danielrech@hotmail.com>
2018-03-15 20:05:01 -0300

    DatastoreProducer.java: producer for a Morphia Datastore EntityPackage.java: marker interface for Morphia entity scanner

---
+++ src/main/java/rech/haeser/daniel/DatastoreProducer.java
@@ -0,0 +1,45 @@
+package rech.haeser.daniel;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.ejb.Singleton;
+import javax.enterprise.inject.Produces;
+
+import org.mongodb.morphia.Datastore;
+import org.mongodb.morphia.Morphia;
+
+import com.mongodb.MongoClient;
+import rech.haeser.daniel.model.EntityPackage;
+
+/**
+ * @author daniel.rech
+ */
+@Singleton
+public class DatastoreProducer {
+
+    private MongoClient mongoClient;
+
+    @PostConstruct
+    public void postConstruct() {
+        mongoClient = new MongoClient();
+    }
+
+    @Produces
+    @javax.inject.Singleton
+    public Datastore produceDatastore() {
+
+        final Morphia morphia = new Morphia();
+        morphia.mapPackage(EntityPackage.class.getPackage().getName());
+
+        final Datastore datastore = morphia.createDatastore(mongoClient, "userapp"); // FIXME dbName
+        datastore.ensureIndexes();
+        return datastore;
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        if (mongoClient != null) {
+            mongoClient.close();
+        }
+    }
+}
---
+++ src/main/java/rech/haeser/daniel/model/EntityPackage.java
@@ -0,0 +1,9 @@
+package rech.haeser.daniel.model;
+
+/**
+ * This is a marker interface for Morphia's entity scanning.
+ *
+ * @author daniel.rech
+ */
+public interface EntityPackage {
+}
