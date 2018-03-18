59a3d08cb99b9f17aa25b164588ba31770c262ec
Daniel Haeser Rech <danielrech@hotmail.com>
2018-03-18 15:42:07 -0300

    User.java: added text index on 'name' and 'address', and unique index on 'email'

--- src/main/java/rech/haeser/daniel/model/User.java
+++ src/main/java/rech/haeser/daniel/model/User.java
@@ -1,5 +1,7 @@
 package rech.haeser.daniel.model;
 
+import static org.mongodb.morphia.utils.IndexType.TEXT;
+
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -7,12 +9,21 @@ import org.bson.types.ObjectId;
 import org.hibernate.validator.constraints.Email;
 import org.mindrot.jbcrypt.BCrypt;
 import org.mongodb.morphia.annotations.Entity;
+import org.mongodb.morphia.annotations.Field;
 import org.mongodb.morphia.annotations.Id;
+import org.mongodb.morphia.annotations.Index;
+import org.mongodb.morphia.annotations.IndexOptions;
+import org.mongodb.morphia.annotations.Indexed;
+import org.mongodb.morphia.annotations.Indexes;
 
 /**
  * @author daniel.rech
  */
 @Entity
+@Indexes(@Index(fields = {
+        @Field(value = "name", type = TEXT),
+        @Field(value = "address", type = TEXT)
+}))
 public class User {
 
     private static final int MINIMUM_EMAIL_LENGTH = 3;
@@ -31,6 +42,7 @@ public class User {
     @Email
     @Size(min = MINIMUM_EMAIL_LENGTH, max = MAXIMUM_EMAIL_LENGTH)
     @NotNull
+    @Indexed(options = @IndexOptions(unique = true))
     private String email;
 
     @NotNull
